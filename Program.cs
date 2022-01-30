de4be1f791ef11ec8d2d471abe30a1d2edb72b5c
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-01-30 13:01:21 +0530

    Aaryan adding EmployeePayroll.Ado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayroll_Ado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Emp Payroll");
+        }
+    }
+}
